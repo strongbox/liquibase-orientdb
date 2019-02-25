12dc523b18e178e2d7c885006e67dab4224aec6f
sbespalov <serge.bespalov@gmail.com>
2019-02-25 08:45:49 +0700

    Liquibase version upgrade (3.5.3->3.6.3)

--- src/main/java/liquibase/datatype/ext/OrientDataType.java
+++ src/main/java/liquibase/datatype/ext/OrientDataType.java
@@ -1,11 +1,19 @@
 package liquibase.datatype.ext;
 
-import liquibase.datatype.LiquibaseDataType;
-
 import org.unbrokendome.liquibase.orientdb.common.OrientPropertyType;
+
+import liquibase.change.core.LoadDataChange.LOAD_DATA_TYPE;
+import liquibase.datatype.LiquibaseDataType;
 
 
 public abstract class OrientDataType extends LiquibaseDataType {
 
 	public abstract OrientPropertyType toOrientPropertyType();
+	
+    @Override
+    public LOAD_DATA_TYPE getLoadTypeName()
+    {
+        return null;
+    }
+
 }
